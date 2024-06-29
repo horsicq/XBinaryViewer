6a858908351ee359d3371f168b0c1d6e009ed764
horsicq <horsicq@gmail.com>
2024-06-30 00:42:47 +0200

    Update file(s): 2024-06-30

--- src/gui/guimainwindow.cpp
+++ src/gui/guimainwindow.cpp
@@ -43,7 +43,9 @@ GuiMainWindow::GuiMainWindow(QWidget *pParent) : QMainWindow(pParent), ui(new Ui
     g_xOptions.addID(XOptions::ID_VIEW_QSS, "");
     g_xOptions.addID(XOptions::ID_VIEW_LANG, "System");
     g_xOptions.addID(XOptions::ID_VIEW_FONT_CONTROLS, XOptions::getDefaultFont().toString());
-    g_xOptions.addID(XOptions::ID_VIEW_FONT_TABLES, XOptions::getMonoFont().toString());
+    g_xOptions.addID(XOptions::ID_VIEW_FONT_TABLEVIEWS, XOptions::getMonoFont().toString());
+    g_xOptions.addID(XOptions::ID_VIEW_FONT_TREEVIEWS, XOptions::getDefaultFont().toString());
+    g_xOptions.addID(XOptions::ID_VIEW_FONT_TEXTEDITS, XOptions::getMonoFont().toString());
     g_xOptions.addID(XOptions::ID_VIEW_STAYONTOP, false);
     g_xOptions.addID(XOptions::ID_VIEW_SHOWLOGO, false);
     g_xOptions.addID(XOptions::ID_FILE_SAVELASTDIRECTORY, true);

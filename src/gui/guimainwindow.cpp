f75935c2d2064b9aff1284e7e21464ab395362c4
horsicq <horsicq@gmail.com>
2025-02-27 08:47:11 +0100

    Update file(s): 2025-02-27

--- src/gui/guimainwindow.cpp
+++ src/gui/guimainwindow.cpp
@@ -225,7 +225,7 @@ void GuiMainWindow::processFile(const QString &sFileName)
                 formatOptions.vmode = XFW_DEF::VMODE_FILETYPE;
                 // formatOptions.nStartType = SBINARY::TYPE_INFO;
                 ui->widgetViewer->setData(g_pFile, formatOptions);
-                ui->widgetViewer->setXInfoDB(g_pXInfo, "main");
+                ui->widgetViewer->setXInfoDB(g_pXInfo, XInfoDB::PROFILE_MAIN);
 
                 ui->widgetViewer->reload();
 
